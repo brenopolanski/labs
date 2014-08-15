121584675df3fb4d164c7f56084ae74e0be16353
breno.polanski <breno.polanski@gmail.com>
2014-08-15 10:50:44 -0500

    Add example unix datestamp

---
+++ python/basic/unix2datestamp.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+from time import mktime
+
+date = 'Today is Monday, August 15th 2014'
+
+unixTime = datetime.strptime(date, 'Today is %A, %B %dth %Y')
+
+converted = mktime(u)
+
+print(unixTime)
