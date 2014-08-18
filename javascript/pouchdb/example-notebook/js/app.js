4b8b88a508adcc408c3fea050d62f083329c4327
Breno Polanski <breno.polanski@gmail.com>
2014-08-18 13:37:26 -0300

    Update app.js

--- javascript/pouchdb/example-notebook/js/app.js
+++ javascript/pouchdb/example-notebook/js/app.js
@@ -1,9 +1,36 @@
 var APP = (function() {
 	'use strict';
 
+	var db = new PouchDB('notebook');
+
 	var APP = {
 		init: function() {
-			console.log('ihuuu')
+			APP.eventBtn();
+		},
+
+		addContact: function() {
+			var _name = document.querySelector('#name').value,
+				_email = document.querySelector('#email').value,
+				_phone = document.querySelector('#phone').value;
+
+			var contact = {
+				_id: _email,
+				name: _name,
+				phone: _phone
+			};
+
+			db.put(contact, function(err, res) {
+				if(!err) {
+					alert('Contact successfully registered!');
+					document.location.reload();
+				}
+			});
+		},
+
+		eventBtn: function() {
+			var btn = document.querySelector('#btn-save');
+
+			btn.addEventListener('click', this.addContact, false);
 		}
 	};
 
