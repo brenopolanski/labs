13c82fae07192cf4da08678df35e9eb1b0e2825b
breno.polanski <breno.polanski@gmail.com>
2014-08-20 10:40:23 -0500

    Delete folder nodejs

--- nodejs/basic/hello-http.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-http.createServer(function(req, res) {
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.end('<h1>Hello World</h1>');
-}).listen(1337, '127.0.0.1');
-
-console.log('Server running at http://127.0.0.1:1337/');
