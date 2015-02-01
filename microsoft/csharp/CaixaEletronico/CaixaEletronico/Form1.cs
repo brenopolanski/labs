c640a33df23927e027cb2b6111c897601a522046
breno.polanski <breno.polanski@gmail.com>
2015-02-01 20:58:41 -0300

    Add simple examples

---
+++ microsoft/csharp/CaixaEletronico/CaixaEletronico/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaixaEletronico
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int numeroDaConta;
+            numeroDaConta = 1;
+
+            double saldo = 100.0;
+            double valor = 10.0;
+            double saldoAposSaque = saldo - valor;
+
+            MessageBox.Show("O saldo atual é: " + saldoAposSaque);
+        }
+    }
+}
