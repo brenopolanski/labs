009b505cb7c049e62a4bae8ad6ace1501ed89e87
breno.polanski <breno.polanski@gmail.com>
2015-03-04 15:29:38 -0600

    Add example split hexadecimal

---
+++ microsoft/csharp/HexaECG/HexaECG/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexaECG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string escapeBegin = "55AA";
+            string escapeEnd = "FC00";
+            string hexa = "55AA078053FD18FCC5FE4B0347FF6FFD000000000000000000000000FC00";
+            int pos = hexa.IndexOf("55AA");
+            int tamDelimiter = 4;
+            int len = hexa.Length;
+
+            if (hexa.IndexOf(escapeBegin) > -1) 
+            {
+                while (len > 0) 
+                {
+                    Console.WriteLine(hexa.Substring(0, tamDelimiter));
+                    hexa = hexa.Substring(tamDelimiter);
+                    len = hexa.Length;
+                }
+            }
+        }
+    }
+}
