aabb4c603e7d242fa000d6ad9ac1adae60b847c8
breno.polanski <breno.polanski@gmail.com>
2015-03-04 17:08:32 -0600

    Add example of conversion hexa to binary

---
+++ microsoft/csharp/HexBinECG/HexBinECG/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexBinECG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hexa = "55AA078053FD18FCC5FE4B0347FF6FFD000000000000000000000000FC00";
+            string escapeBegin = "55AA";
+            string escapeEnd = "FC00";
+            int tamDelimiter = 4;
+            int len = hexa.Length;
+            string strHexa;
+            string strBin;
+
+            if (hexa.IndexOf(escapeBegin) > -1)
+            {
+                hexa = hexa.Substring(tamDelimiter);
+
+                while (len > 0)
+                {
+                    strHexa = hexa.Substring(0, tamDelimiter);
+
+                    if (!strHexa.Equals(escapeBegin) || !strHexa.Equals(escapeEnd))
+                    {
+                        strBin = Convert.ToString(Convert.ToInt32(strHexa, 16), 2);
+                        Console.WriteLine(strBin);
+                        hexa = hexa.Substring(tamDelimiter);
+                        len = hexa.Length;
+                    }
+                }
+            }
+        }
+    }
+}
