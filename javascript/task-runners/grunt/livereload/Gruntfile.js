838b5684efb09d247e02a60da2c1649ad325bb55
breno.polanski <breno.polanski@gmail.com>
2015-05-28 18:48:40 -0300

    Update repository

---
+++ javascript/task-runners/grunt/livereload/Gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) {
+
+    "use strict"
+
+    // Configuração das tarefas
+    // ---------------------------------------
+    grunt.initConfig({
+
+        // Tarefa watch
+        watch: {
+            options: {
+                livereload: true
+            },
+            js: {
+                files: "js/*.js"
+                // tasks: ["uglify", "jshint"]
+            },
+            html: {
+                files: "./*.html"
+            }
+        },
+
+        // Tarefa connect
+        connect: {
+            server: {
+                options: {
+                    port: 9000,
+                    base: ".",
+                    hostname: "localhost",
+                    livereload: true,
+                    open: true
+                }
+            }
+        }
+
+    });
+
+
+    // Carregando os plugins
+    // ---------------------------------------
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+
+
+    // Registrando a tarefa customizada
+    // ---------------------------------------
+    grunt.registerTask( "default", [ "connect", "watch" ]);
+
+};
---
+++ javascript/task-runners/grunt/livereload/js/main.js
@@ -0,0 +1,3 @@
+var str = 'Hello World!';
+
+console.log(str);
