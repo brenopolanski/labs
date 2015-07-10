1968ec8ec178d3d19a0f135b30b22cb66717453c
breno.polanski <breno.polanski@gmail.com>
2015-07-10 10:01:54 -0500

    Update serialport-js

--- javascript/node/serialportjs/index.js
+++ javascript/node/serialportjs/index.js
@@ -1,13 +1,7 @@
-var serialport = require('serialport');
-var SerialPort = serialport.SerialPort;
+var serialjs = require('serialport-js');
 
-console.log(serialport);
-
-// list serial ports:
-serialport.list(function (err, ports) {
-  console.log(err);
-  console.log(ports);
-  ports.forEach(function(port) {
-    console.log(port.comName);
-  });
-});
+serialjs.find(
+    function(ports) {
+        console.log('available usb serial : ', ports);
+    }
+);
