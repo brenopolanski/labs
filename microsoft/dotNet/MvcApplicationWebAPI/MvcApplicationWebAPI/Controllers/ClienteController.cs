5acc6699f1779bece94320fb597dffcbe20ce978
breno.polanski <breno.polanski@gmail.com>
2015-07-20 10:32:32 -0500

    Add example ASP.Net Web API 96 Meu primeiro serviço REST

---
+++ microsoft/dotNet/MvcApplicationWebAPI/MvcApplicationWebAPI/Controllers/ClienteController.cs
@@ -0,0 +1,49 @@
+﻿using MvcApplicationWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MvcApplicationWebAPI.Controllers
+{
+    public class ClienteController : ApiController
+    {
+        private readonly Cliente[] Clientes = new Cliente[]
+        {
+            new Cliente { ID = 1, Nome = "Eduardo Pires", Email = "falecom@eduardopires.net.br", Ativo = true },
+            new Cliente { ID = 2, Nome = "Bill Gates", Email = "gates@microsoft.com", Ativo = true },
+            new Cliente { ID = 3, Nome = "Aleister Crowley", Email = "aleister@therion.com", Ativo = false }
+        };
+
+        // GET api/cliente
+        public Cliente[] Get()
+        {
+            return Clientes;
+        }
+
+        // GET api/cliente/5
+        public Cliente Get(int id)
+        {
+            var clientes = Clientes;
+
+            return clientes.SingleOrDefault(x => x.ID == id);
+        }
+
+        // POST api/cliente
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/cliente/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/cliente/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
