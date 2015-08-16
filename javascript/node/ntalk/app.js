e8993f39a605096171b3182135ef8d4a5ad57aeb
breno.polanski <breno.polanski@gmail.com>
2015-08-16 18:08:24 -0300

    Project done

--- javascript/node/ntalk/app.js
+++ javascript/node/ntalk/app.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var load = require('express-load');
-var app = express;
+var app = express();
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
-load.('models')
+// É importante colocar em ordem os recursos a serem carregados pela função load().
+// Neste caso, os models são carregados primeiro, para que os controllers possam
+// usá-las, e por último, os routes usarem toda lógica de seus controllers.
+load('models')
   .then('controllers')
   .then('routes')
-  .into('app');
+  .into(app);
 
 app.listen(3000, function() {
   console.log('Ntalk no ar.');
