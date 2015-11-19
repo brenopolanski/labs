b998d1b763e1d22da1ed298c6edad7c9e5b0cd2a
breno.polanski <breno.polanski@gmail.com>
2015-11-19 15:16:03 -0300

    Add thigs of React

--- javascript/react/catch-of-the-day/7/scripts/main.js
+++ javascript/react/catch-of-the-day/7/scripts/main.js
@@ -1,5 +1,13 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+var React       = require('react');
+var ReactDOM    = require('react-dom');
+
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var Navigation = ReactRouter.Navigation;
+var createBrowserHistory = require('history/lib/createBrowserHistory');
+
+var h = require('./helpers');
 
 /*
 	App
@@ -71,11 +79,32 @@ var StorePicker = React.createClass({
 		return (
 			<form className="store-selector">
 				<h2>Please Enter a Store</h2>
-				<input type="text" ref="storeId" required />
+				<input type="text" ref="storeId" defaultValue={h.getFunName()} required />
 				<input type="submit" />
 			</form>
 		)
 	}
 });
 
-ReactDOM.render(<App />, document.querySelector('#main'));
+/*
+	Not Found
+ */
+var NotFound = React.createClass({
+	render: function() {
+		return <h1>Not Found!</h1>
+	}
+});
+
+/*
+	Routes
+ */
+var routes = (
+	<Router history={createBrowserHistory()}>
+		<Route path="/" component={StorePicker} />
+		<Route path="/store/:storeId" component={App} />
+		<Route path="*" component={NotFound} />
+	</Router>
+);
+
+// ReactDOM.render(<App />, document.querySelector('#main'));
+ReactDOM.render(routes, document.querySelector('#main'));
