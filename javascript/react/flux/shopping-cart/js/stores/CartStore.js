116519eb1455992f4a812e3104969ead7ec61521
breno.polanski <breno.polanski@gmail.com>
2016-03-06 00:07:04 -0300

    Add initial example React using Flux

---
+++ javascript/react/flux/shopping-cart/js/stores/CartStore.js
@@ -0,0 +1,3 @@
+'use strict';
+
+let AppDispatcher = require('../dispatcher/AppDispatcher');
---
+++ javascript/react/flux/shopping-cart/js/stores/ProductsStore.js
@@ -0,0 +1,54 @@
+'use strict';
+
+let AppDispatcher = require('../dispatcher/AppDispatcher');
+let EventEmitter = require('events').EventEmitter;
+let CartConstants = require('../constants/CartConstants');
+
+// Initial data points
+let _products = [];
+
+// will load products from some API
+function loadProducts(data) {
+  _products = data;
+}
+
+class ProductStoreFactory extends EventEmitter {
+
+  // Return Product list
+  getProducts() {
+    return _products;
+  }
+
+  // Emit Change event
+  emitChange() {
+    this.emit('change');
+  }
+
+  // Add change listener
+  addChangeListener(callback) {
+    this.on('change', callback);
+  }
+
+  // Remove change listener
+  removeChangeListener(callback) {
+    this.removeListener('change', callback);
+  }
+}
+
+let ProductStore = new ProductStoreFactory();
+
+AppDispatcher.register(function(payload) {
+  let action = payload.action;
+
+  switch (action.actionType) {
+    case CartConstants.LOAD_PRODUCTS:
+      loadProducts(action.data);
+      break;
+    default:
+      return;
+  }
+
+  ProductStore.emitChange();
+});
+
+module.exports = ProductStore;
