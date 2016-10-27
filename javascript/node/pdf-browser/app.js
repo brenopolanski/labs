c815dcde418db35dcc9fddcc04ae6fd22167a24e
brenopolanski <breno.polanski@gmail.com>
2016-10-27 11:25:37 -0300

    Add example 2 using PDF.js

--- javascript/node/pdf-browser/app.js
+++ javascript/node/pdf-browser/app.js
@@ -1,6 +1,9 @@
 var express = require('express');
+var cors = require('cors');
 var fs = require('fs');
 var app = express();
+
+app.use(cors());
 
 app.get('/', function(req, res) {
   var filePath = '/report.pdf';
