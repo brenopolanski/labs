0a6df870b2ed9aca9cbee7d86e67dcf64cb77fd2
brenopolanski <breno.polanski@gmail.com>
2016-12-28 19:00:17 -0300

    Update app react + redux

--- javascript/react/Learn-Redux-Starter-Files/learn-redux/client/reducers/posts.js
+++ javascript/react/Learn-Redux-Starter-Files/learn-redux/client/reducers/posts.js
@@ -4,10 +4,22 @@
 // 2. copy of current state
 
 function posts(state = [], action) {
-  console.log('The post will change');
-  console.log(state, action);
+  // console.log('The post will change');
+  // console.log(state, action);
 
-  return state;
+  switch(action.type) {
+    case 'INCREMENT_LIKES':
+      console.log('Incrementing Likes!!');
+      const i = action.index;
+
+      return [
+        ...state.slice(0, i), // before the one we are updating
+        {...state[i], likes: state[i].likes + 1},
+        ...state.slice(i + 1) // after the one we are updating
+      ]
+    default:
+      return state;
+  }
 }
 
 export default posts;
--- javascript/react/Learn-Redux-Starter-Files/learn-redux/client/store.js
+++ javascript/react/Learn-Redux-Starter-Files/learn-redux/client/store.js
@@ -1,4 +1,4 @@
-import { createStore, compse } from 'redux';
+import { createStore, compose } from 'redux';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { browserHistory } from 'react-router';
 
@@ -14,8 +14,20 @@ const defaultState = {
   comments
 };
 
-const store = createStore(rootReducer, defaultState);
+const enhancers = compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+);
+
+const store = createStore(rootReducer, defaultState, enhancers);
 
 export const history = syncHistoryWithStore(browserHistory, store);
+
+// Hot reloading redux reducers with webpack
+if (module.hot) {
+  module.hot.accept('./reducers/', () => {
+    const nextRootReducer = require('./reducers/index').default;
+    store.replaceReducer(nextRootReducer);
+  });
+}
 
 export default store;
