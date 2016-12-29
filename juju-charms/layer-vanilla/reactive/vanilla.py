7bcfff5ea20323cc8ae6916a12f1511cfe868acf
brenopolanski <breno.polanski@gmail.com>
2016-12-29 20:52:08 -0300

    Update layer

---
+++ juju-charms/layer-vanilla/reactive/vanilla.py
@@ -0,0 +1,53 @@
+import pwd
+import os
+from charmhelpers.core.hookenv import (
+    status_set,
+    log
+)
+from charmhelpers.core.templating import render
+from charms.reactive import (
+    when,
+    when_not
+)
+from charms.reactive import (
+    set_state,
+    remove_state
+)
+
+
+@when('apache.available', 'database.available')
+def setup_vanilla(mysql):
+    render(
+        source='vanilla_config.php',
+        target='/var/www/vanilla/conf/config.php',
+        owner='www-data',
+        perms=0o777,
+        context={
+            'db': mysql
+        }
+    )
+    uid = pwd.getpwnam('www-data').pw_uid
+    log('LOG UID >>> ' + uid)
+    os.chown('/var/www/vanilla/cache', uid, -1)
+    os.chown('/var/www/vanilla/uploads', uid, -1)
+    set_state('apache.start')
+    status_set('maintenance', 'Starting Apache')
+
+
+@when('apache.available')
+@when_not('database.connected')
+def missing_mysql():
+    remove_state('apache.start')
+    status_set('blocked', 'Please add relation to MySQL')
+
+
+@when('database.connected')
+@when_not('database.available')
+def waiting_mysql(mysql):
+    remove_state('apache.start')
+    status_set('waiting', 'Waiting for MySQL')
+
+
+@when('apache.started')
+def started():
+    status_set('active', 'Ready')
---
+++ juju-charms/layer-vanilla/templates/vanilla_config.php
@@ -0,0 +1,7 @@
+<?php
+  if (!defined('APPLICATION')) exit();
+  $Configuration['Database']['Host'] = '{{ db.host() }}';
+  $Configuration['Database']['Name'] = '{{ db.database() }}';
+  $Configuration['Database']['User'] = '{{ db.user() }}';
+  $Configuration['Database']['Password'] = '{{ db.password() }}';
+?>
