e958adcb10117e660ecdc1f1eb3fc62fb6eefc1e
brenopolanski <breno.polanski@gmail.com>
2017-06-04 22:01:20 -0300

    Add input events

--- javascript/phaser/farm-animals-lessons/00-game-development/js/main.js
+++ javascript/phaser/farm-animals-lessons/00-game-development/js/main.js
@@ -7,6 +7,7 @@ var GameState = {
     this.load.image('horse', 'assets/images/horse.png');
     this.load.image('pig', 'assets/images/pig.png');
     this.load.image('sheep', 'assets/images/sheep3.png');
+    this.load.image('arrow', 'assets/images/arrow.png');
   },
 
   //executed after everything is loaded
@@ -19,34 +20,46 @@ var GameState = {
     // create a sprite for the background
     this.background = this.game.add.sprite(0, 0, 'background');
 
-    // center of the world
-    this.chicken = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'chicken');
+    this.pig = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'pig');
+    this.pig.anchor.setTo(0.5, 0.5);
 
-    // place a sprite by it's center, not the top-left corner
-    this.chicken.anchor.setTo(0.5, 0.5); // or just this.chicken.anchor.setTo(0.5)
+    this.pig.inputEnabled = true;
+    this.pig.input.pixelPerfectClick = true;
+    this.pig.events.onInputDown.add(this.animateAnimal, this);
 
-    // increate it's width by 2, keep same height
-    this.chicken.scale.setTo(2, 1);
+    // left arrow
+    this.leftArrow = this.game.add.sprite(60, this.game.world.centerY, 'arrow');
+    this.leftArrow.anchor.setTo(0.5);
+    this.leftArrow.scale.x = -1;
+    this.leftArrow.customParams = { direction: -1 };
 
-    this.horse = this.game.add.sprite(120, 10, 'horse');
-    // decreate width and height to half
-    this.horse.scale.setTo(0.5);
+    // left arrow allow user input
+    this.leftArrow.inputEnabled = true;
+    this.leftArrow.input.pixelPerfectClick = true;
+    this.leftArrow.events.onInputDown.add(this.switchAnimal, this);
 
-    this.pig = this.game.add.sprite(500, 300, 'pig');
-    this.pig.anchor.setTo(0.5);
-    // flip on x
-    this.pig.scale.setTo(-1, 1);
+    // right arrow
+    this.rightArrow = this.game.add.sprite(580, this.game.world.centerY, 'arrow');
+    this.rightArrow.anchor.setTo(0.5);
+    this.rightArrow.customParams = { direction: 1 };
 
-    this.sheep = this.game.add.sprite(100, 250, 'sheep');
-    this.sheep.scale.setTo(0.5);
-    this.sheep.anchor.setTo(0.5);
-    this.sheep.angle = -45;
+    // right arrow allow user input
+    this.rightArrow.inputEnabled = true;
+    this.rightArrow.input.pixelPerfectClick = true;
+    this.rightArrow.events.onInputDown.add(this.switchAnimal, this);
   },
 
   //this is executed multiple times per second
   update: function() {
-    this.sheep.angle += 0.5;
   },
+
+  switchAnimal: function(sprite, event) {
+    console.log('animal');
+  },
+
+  animateAnimal: function(sprite, event) {
+    console.log('animal');
+  }
 };
 
 //initiate the Phaser framework
