426c4e62dbae72219605013e7ead6c86729087af
brenopolanski <breno.polanski@gmail.com>
2017-08-16 11:31:18 -0300

    Add example using node

---
+++ javascript/node/parameters/output.js
@@ -0,0 +1,12 @@
+// Newer versions of node.js use the lower-case argv
+var argv = process.ARGV || process.argv;
+
+// {"apiVersion":"1.0","dataSourceName":"signoff","tables":[{"id":"id_tbl_0","name":"signoff1","sourceTable":"signoff1","keyColumn":"%Entity A"}],"measures":[{"id":"id_measure_0","name":"Direction","properties":{"sourceColumn":"Direction","defaultAggregation":"sum"}},{"id":"id_measure_1","name":"Call duration","properties":{"sourceColumn":"Call Duration","defaultAggregation":"sum"}}]}
+
+// console.log(typeof argv);
+// console.log(argv[2]);
+
+// var data = JSON.parse(argv[2]);
+var data = JSON.stringify({"apiVersion":"1.0","dataSourceName":"signoff","tables":[{"id":"id_tbl_0","name":"signoff1","sourceTable":"signoff1","keyColumn":"%Entity A"}],"measures":[{"id":"id_measure_0","name":"Direction","properties":{"sourceColumn":"Direction","defaultAggregation":"sum"}},{"id":"id_measure_1","name":"Call duration","properties":{"sourceColumn":"Call Duration","defaultAggregation":"sum"}}]});
+
+console.log(data);
---
+++ javascript/node/parameters/test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+output=$(node ./output.js);
+echo "=========================";
+echo "$output";
+echo "=========================";
