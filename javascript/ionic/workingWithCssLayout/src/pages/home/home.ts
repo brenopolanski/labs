fba16be088c5cc44e1773c0296a42ae2cbd953a4
brenopolanski <breno.polanski@gmail.com>
2017-09-14 23:30:04 -0300

    Working with CSS

---
+++ javascript/ionic/workingWithCssLayout/src/pages/home/home.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  takePicture() {
+    console.log('takePicture');
+  }
+
+  speakAgain() {
+    console.log('speakAgain');
+  }
+
+}
