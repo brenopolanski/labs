0111c0110d0de414e0b85f292f4a18c6e15b8243
brenopolanski <breno.polanski@gmail.com>
2017-10-29 14:27:07 -0300

    Add example using Next.js

---
+++ javascript/nextjs/hello-next/.next/5.53dc63613e607cdda75d.hot-update.js
@@ -0,0 +1,48 @@
+webpackHotUpdate(5,{
+
+/***/ 100:
+false,
+
+/***/ 127:
+false,
+
+/***/ 17:
+false,
+
+/***/ 31:
+false,
+
+/***/ 33:
+false,
+
+/***/ 34:
+false,
+
+/***/ 396:
+/***/ (function(module, exports) {
+
+"use strict";
+throw new Error("Module build failed: SyntaxError: Unterminated JSX contents (4:8)\n\n\u001b[0m \u001b[90m 2 | \u001b[39m  \u001b[33m<\u001b[39m\u001b[33mdiv\u001b[39m\u001b[33m>\u001b[39m\n \u001b[90m 3 | \u001b[39m    \u001b[33m<\u001b[39m\u001b[33mp\u001b[39m\u001b[33m>\u001b[39m\u001b[33mHello\u001b[39m \u001b[33mNext\u001b[39m\u001b[33m.\u001b[39mjs\n\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 4 | \u001b[39m  \u001b[33m<\u001b[39m\u001b[33m/\u001b[39m\u001b[33mdiv\u001b[39m\u001b[33m>\u001b[39m\n \u001b[90m   | \u001b[39m        \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\n \u001b[90m 5 | \u001b[39m)\n \u001b[90m 6 | \u001b[39m\n \u001b[90m 7 | \u001b[39m\u001b[36mexport\u001b[39m \u001b[36mdefault\u001b[39m \u001b[33mIndex\u001b[39m\u001b[0m\n");
+
+/***/ }),
+
+/***/ 57:
+false,
+
+/***/ 58:
+false,
+
+/***/ 69:
+false,
+
+/***/ 84:
+false,
+
+/***/ 86:
+false,
+
+/***/ 99:
+false
+
+})
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiNS41M2RjNjM2MTNlNjA3Y2RkYTc1ZC5ob3QtdXBkYXRlLmpzIiwic291cmNlcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7O0EiLCJzb3VyY2VSb290IjoiIn0=
