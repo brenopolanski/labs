41855c117f9f8822e39223ca0fcd674f1bae15ba
brenopolanski <breno.polanski@gmail.com>
2018-04-19 11:37:40 -0300

    Update code

--- aws/s3-examples/node/AWS_S3_File_Upload.js
+++ aws/s3-examples/node/AWS_S3_File_Upload.js
@@ -1,24 +1,28 @@
-var AWS = require('aws-sdk'),
-    fs = require('fs');
+const AWS = require('aws-sdk');
+const fs = require('fs');
 
-// For dev purposes only
-AWS.config.update({ accessKeyId: process.env.ACCESS_KEY_ID, secretAccessKey: process.env.SECRET_ACCESS_KEY });
+AWS.config.update({
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY
+});
 
 // Read in the file, convert it to base64, store to S3
-fs.readFile('del.txt', function (err, data) {
-  if (err) { throw err; }
+fs.readFile('del.txt', (err, data) => {
+  if (err) {
+    throw err;
+  }
 
-  var base64data = new Buffer(data, 'binary');
+  const base64data = new Buffer(data, 'binary');
+  const s3 = new AWS.S3();
 
-  var s3 = new AWS.S3();
   s3.upload({
     Bucket: 'brenopolanski.com',
     Key: 'del2.txt',
     Body: base64data,
     ACL: 'public-read'
-  },function (resp) {
+  },
+  resp => {
     console.log(arguments);
-    console.log('Successfully uploaded package.');
+    console.log('Successfully uploaded package!');
   });
-
 });
