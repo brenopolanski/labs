d3bc62615101136da7202c4346bbfaf9c854b40f
brenopolanski <breno.polanski@gmail.com>
2018-06-04 18:29:12 -0300

    Create ContactList component

--- javascript/react/react-apz/src/App.js
+++ javascript/react/react-apz/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import ContactList from './containers/ContactList';
 import './App.css';
 
 class App extends Component {
@@ -11,7 +12,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <ContactList />
         </p>
       </div>
     );
---
+++ javascript/react/react-apz/src/containers/ContactList.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class ContactList extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Contacts</h1>
+        <table className="table table-striped table-responsive">
+          <thead>
+            <th>Name</th>
+            <th>Phone</th>
+            <th>Email</th>
+          </thead>
+          <tbody>
+            {
+              this.props.contactList.map(({ name, phone, email }, index) => (
+                <tr key={index}>
+                  <td>{name}</td>
+                  <td>{phone}</td>
+                  <td>{email}</td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  ...state.contacts
+});
+
+const mapDispatchToProps = dispatch => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ContactList);
