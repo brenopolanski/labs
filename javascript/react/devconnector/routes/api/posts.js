2a8e39ef697fa608103e323915fc408d245892dc
brenopolanski <breno.polanski@gmail.com>
2018-06-21 16:02:35 -0300

    Create API and Model User

--- javascript/react/devconnector/routes/api/posts.js
+++ javascript/react/devconnector/routes/api/posts.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // @route  GET api/posts/test
-// @desc   Tests post route
+// @desc   Tests posts route
 // @access Public
 router.get('/test', (req, res) => res.json({ msg: 'Posts Works' }));
 
