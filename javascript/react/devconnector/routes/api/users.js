4cda4cc4fd727beb6ded7b86918d6ed0481c9ccc
brenopolanski <breno.polanski@gmail.com>
2018-07-02 18:31:06 -0300

    Creating The JWT

--- javascript/react/devconnector/routes/api/users.js
+++ javascript/react/devconnector/routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 // Load User model
 const User = require('../../models/User');
@@ -46,6 +48,53 @@ router.post('/register', (req, res) => {
         });
       });
     }
+  });
+});
+
+// @route  POST api/users/login
+// @desc   Login User / Returning JWT Token
+// @access Public
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({ email }).then(user => {
+    // Check for user
+    if (!user) {
+      return res.status(404).json({ email: 'User not found' });
+    }
+
+    // Check Password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        // res.json({ msg: 'Success' });
+
+        // User Matched
+
+        // Create JWT Payload
+        const payload = {
+          id: user.id,
+          name: user.name,
+          avatar: user.avatar
+        };
+
+        // Sign Token
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: 'Bearer ' + token
+            });
+          }
+        );
+      } else {
+        return res.status(400).json({ password: 'Password incorrect' });
+      }
+    });
   });
 });
 
