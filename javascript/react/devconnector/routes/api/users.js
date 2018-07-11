8c7b535f43e34e9742ae87066bdfa02b94ddf23b
brenopolanski <breno.polanski@gmail.com>
2018-07-11 10:11:32 -0300

    Add validation

--- javascript/react/devconnector/routes/api/users.js
+++ javascript/react/devconnector/routes/api/users.js
@@ -6,6 +6,9 @@ const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
 
+// Load Input Validation
+const validateRegisterInput = require('../../validation/register');
+
 // Load User model
 const User = require('../../models/User');
 
@@ -18,6 +21,13 @@ router.get('/test', (req, res) => res.json({ msg: 'Users Works' }));
 // @desc   Register user
 // @access Public
 router.post('/register', (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
       return res.status(400).json({ email: 'Email already exists' });
