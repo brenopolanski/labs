70fb71ce6887fd70c739e4f492dc5acf6e723fc2
brenopolanski <breno.polanski@gmail.com>
2018-08-01 22:23:51 -0300

    Add examples

--- javascript/react/devconnector/routes/api/profile.js
+++ javascript/react/devconnector/routes/api/profile.js
@@ -1,9 +1,73 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Load Profile Model
+const Profile = require('../../models/Profile');
+
+// Load User Profile
+const User = require('../../models/User');
 
 // @route  GET api/profile/test
 // @desc   Tests profile route
 // @access Public
 router.get('/test', (req, res) => res.json({ msg: 'Profile Works' }));
+
+// @route  GET api/profile/:id
+// @desc   Get current users profile
+// @access Private
+router.get(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = 'There is no profile for this user';
+
+          return res.status(404).json(errors);
+        }
+
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
+// @route  POST api/profile/:id
+// @desc   Create user profile
+// @access Private
+router.post(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    // Get fields
+    const profileFields = {};
+
+    profileFields.user = req.user.id;
+
+    if (req.body.handle) profileFields.handle = req.body.handle;
+    if (req.body.company) profileFields.company = req.body.company;
+    if (req.body.website) profileFields.website = req.body.website;
+    if (req.body.location) profileFields.location = req.body.location;
+    if (req.body.bio) profileFields.bio = req.body.bio;
+    if (req.body.status) profileFields.status = req.body.status;
+    if (req.body.githubusername)
+      profileFields.githubusername = req.body.githubusername;
+
+    // Skills - Split into array
+    if (typeof req.body.skills !== 'undefined') {
+      profileFields.skills = req.body.split(',');
+    }
+
+    // Social
+    profileFields.social = {};
+
+    if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
+  }
+);
 
 module.exports = router;
