f829f943ef0f03b5aec2a70324e6fa337a6a8b89
brenopolanski <breno.polanski@gmail.com>
2019-04-11 21:51:47 -0300

    Add bp3

--- javascript/react/sku-tree-01/src/index.js
+++ javascript/react/sku-tree-01/src/index.js
@@ -12,34 +12,41 @@ const MEASURES_GROUP_COLLAPSED = true;
 
 class TreeNode extends Component {
   state = {
-    isExpanded: !MEASURES_GROUP_COLLAPSED
+    isExpanded: MEASURES_GROUP_COLLAPSED
   };
 
   render() {
     const { measuresGroup, mGroup } = this.props;
 
     return (
-      <li onClick={() => this.setState({ isExpanded: !this.state.isExpanded })}>
-        <span title={mGroup}>{mGroup}</span>
-        <ul className={this.state.isExpanded ? 'tree-node-expanded' : 'tree-node-hidden'} >
+      <li className="bp3-tree-node" onClick={() => this.setState({ isExpanded: !this.state.isExpanded })}>
+        <div className="bp3-tree-node-content">
+          <span className={`bp3-tree-node-caret ${this.state.isExpanded ? 'bp3-tree-node-caret-open' : 'bp3-tree-node-caret-close'} bp3-icon-standard`}></span>
+          <span className="bp3-tree-node-label" title={mGroup}>{mGroup}</span>
+        </div>
+        <ul className={`bp3-tree-node-list ${this.state.isExpanded ? 'bp3-tree-node-expanded' : 'bp3-tree-node-hidden'}`}>
           {measuresGroup[mGroup].map(measure =>
             (typeof measure['visible'] === 'undefined' ||
               measure['visible']) && (
-              <li key={`${_.uniqueId(`${measure.caption}_`)}`}>
-                <a
-                  title={
-                    measure.description
-                      ? measure.description
-                      : measure.uniqueName
-                  }
-                  href={`#Measures/member/${encodeURIComponent(
-                    measure.uniqueName
-                  )}`}
-                  measure={measure.name}
-                  type="EXACT"
-                >
-                  {measure.caption}
-                </a>
+              <li key={`${_.uniqueId(`${measure.caption}_`)}`} className="bp3-tree-node">
+                <div className="bp3-tree-node-content">
+                  <span className="bp3-tree-node-caret-none bp3-icon-standard"></span>
+                  <a
+                    className="bp3-tree-node-label"
+                    title={
+                      measure.description
+                        ? measure.description
+                        : measure.uniqueName
+                    }
+                    href={`#Measures/member/${encodeURIComponent(
+                      measure.uniqueName
+                    )}`}
+                    measure={measure.name}
+                    type="EXACT"
+                  >
+                    {measure.caption}
+                  </a>
+                </div>
               </li>
             )
           )}
@@ -57,11 +64,13 @@ class App extends Component {
     const measuresGroup = _.groupBy(measures, 'measureGroup');
 
     return (
-      <ul>
-        {Object.keys(measuresGroup).map(key => (
-          <TreeNode key={`${_.uniqueId(`${key}_`)}`} measuresGroup={measuresGroup} mGroup={key} />
-        ))}
-      </ul>
+      <div className="bp3-tree">
+        <ul className="bp3-tree-node-list bp3-tree-root">
+          {Object.keys(measuresGroup).map(key => (
+            <TreeNode key={`${_.uniqueId(`${key}_`)}`} measuresGroup={measuresGroup} mGroup={key} />
+          ))}
+        </ul>
+      </div>
     );
   }
 }
