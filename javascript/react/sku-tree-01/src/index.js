f56040cf3e25545a6743821088a0908465a2dc50
brenopolanski <breno.polanski@gmail.com>
2019-04-11 21:22:56 -0300

    Add tree view example

---
+++ javascript/react/sku-tree-01/src/index.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import _ from 'lodash';
+import initData from './initData';
+
+// Styles
+import '@blueprintjs/core/lib/css/blueprint.css';
+import '@blueprintjs/icons/lib/css/blueprint-icons.css';
+import './index.css';
+
+const MEASURES_GROUP_COLLAPSED = true;
+
+class TreeNode extends Component {
+  state = {
+    isExpanded: !MEASURES_GROUP_COLLAPSED
+  };
+
+  render() {
+    const { measuresGroup, mGroup } = this.props;
+
+    return (
+      <li onClick={() => this.setState({ isExpanded: !this.state.isExpanded })}>
+        <span title={mGroup}>{mGroup}</span>
+        <ul className={this.state.isExpanded ? 'tree-node-expanded' : 'tree-node-hidden'} >
+          {measuresGroup[mGroup].map(measure =>
+            (typeof measure['visible'] === 'undefined' ||
+              measure['visible']) && (
+              <li key={`${_.uniqueId(`${measure.caption}_`)}`}>
+                <a
+                  title={
+                    measure.description
+                      ? measure.description
+                      : measure.uniqueName
+                  }
+                  href={`#Measures/member/${encodeURIComponent(
+                    measure.uniqueName
+                  )}`}
+                  measure={measure.name}
+                  type="EXACT"
+                >
+                  {measure.caption}
+                </a>
+              </li>
+            )
+          )}
+        </ul>
+      </li>
+    );
+  }
+}
+
+class App extends Component {
+  state = initData;
+
+  render() {
+    const { measures } = this.state;
+    const measuresGroup = _.groupBy(measures, 'measureGroup');
+
+    return (
+      <ul>
+        {Object.keys(measuresGroup).map(key => (
+          <TreeNode key={`${_.uniqueId(`${key}_`)}`} measuresGroup={measuresGroup} mGroup={key} />
+        ))}
+      </ul>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ javascript/react/sku-tree-01/src/initData.js
@@ -0,0 +1,94 @@
+const initData = {
+  measures: [
+    {
+      uniqueName: '[Measures].[Unit Sales]',
+      name: 'Unit Sales',
+      caption: 'Unit Sales',
+      dimensionUniqueName: '[Measures]',
+      description: null,
+      levelUniqueName: '[Measures].[MeasuresLevel]',
+      hierarchyUniqueName: '[Measures]',
+      calculated: false,
+      measureGroup: 'Sales'
+    },
+    {
+      uniqueName: '[Measures].[Store Cost]',
+      name: 'Store Cost',
+      caption: 'Store Cost',
+      dimensionUniqueName: '[Measures]',
+      description: null,
+      levelUniqueName: '[Measures].[MeasuresLevel]',
+      hierarchyUniqueName: '[Measures]',
+      calculated: false,
+      measureGroup: 'Sales'
+    },
+    {
+      uniqueName: '[Measures].[Store Sales]',
+      name: 'Store Sales',
+      caption: 'Store Sales',
+      dimensionUniqueName: '[Measures]',
+      description: null,
+      levelUniqueName: '[Measures].[MeasuresLevel]',
+      hierarchyUniqueName: '[Measures]',
+      calculated: false,
+      measureGroup: 'Sales'
+    },
+    {
+      uniqueName: '[Measures].[Sales Count]',
+      name: 'Sales Count',
+      caption: 'Sales Count',
+      dimensionUniqueName: '[Measures]',
+      description: null,
+      levelUniqueName: '[Measures].[MeasuresLevel]',
+      hierarchyUniqueName: '[Measures]',
+      calculated: false,
+      measureGroup: 'Sales'
+    },
+    {
+      uniqueName: '[Measures].[Customer Count]',
+      name: 'Customer Count',
+      caption: 'Customer Count',
+      dimensionUniqueName: '[Measures]',
+      description: null,
+      levelUniqueName: '[Measures].[MeasuresLevel]',
+      hierarchyUniqueName: '[Measures]',
+      calculated: false,
+      measureGroup: 'Sales'
+    },
+    {
+      uniqueName: '[Measures].[Promotion Sales]',
+      name: 'Promotion Sales',
+      caption: 'Promotion Sales',
+      dimensionUniqueName: '[Measures]',
+      description: null,
+      levelUniqueName: '[Measures].[MeasuresLevel]',
+      hierarchyUniqueName: '[Measures]',
+      calculated: false,
+      measureGroup: 'Sales'
+    },
+    {
+      uniqueName: '[Measures].[Profit]',
+      name: 'Profit',
+      caption: 'Profit',
+      dimensionUniqueName: '[Measures]',
+      description: null,
+      levelUniqueName: '[Measures].[MeasuresLevel]',
+      hierarchyUniqueName: '[Measures]',
+      calculated: true,
+      measureGroup: ''
+    },
+    {
+      uniqueName: '[Measures].[Profit Growth]',
+      name: 'Profit Growth',
+      caption: 'Gewinn-Wachstum',
+      dimensionUniqueName: '[Measures]',
+      description: null,
+      levelUniqueName: '[Measures].[MeasuresLevel]',
+      hierarchyUniqueName: '[Measures]',
+      calculated: true,
+      measureGroup: ''
+    }
+  ]
+};
+
+export default initData;
