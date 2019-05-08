d49f7d85cbb11ddf1d74ae29b9875bab6a7223fe
brenopolanski <breno.polanski@gmail.com>
2019-05-08 13:39:09 -0300

    Update react-dates

--- javascript/react/react-dates-airbnb/src/App.js
+++ javascript/react/react-dates-airbnb/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { DateRangePicker } from 'react-dates';
 import 'react-dates/initialize';
+import { DateRangePicker } from 'react-dates';
 import 'react-dates/lib/css/_datepicker.css';
 
 class App extends Component {
@@ -14,12 +14,13 @@ class App extends Component {
     return (
       <DateRangePicker
         startDate={this.state.startDate} // momentPropTypes.momentObj or null,
-        startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
+        startDateId="startDate" // PropTypes.string.isRequired,
         endDate={this.state.endDate} // momentPropTypes.momentObj or null,
-        endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
+        endDateId="endDate" // PropTypes.string.isRequired,
         onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
         focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
         onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
+        isOutsideRange={day => false}
         hideKeyboardShortcutsPanel
         showClearDates
         showDefaultInputIcon
