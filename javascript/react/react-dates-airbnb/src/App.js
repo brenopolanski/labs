9bdeb662326c58bb177e8bd70b2439eb0771186d
brenopolanski <breno.polanski@gmail.com>
2019-05-07 21:04:14 -0300

    Add initial code for react-dates example

---
+++ javascript/react/react-dates-airbnb/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { DateRangePicker } from 'react-dates';
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css';
+
+class App extends Component {
+  state = {
+    startDate: null,
+    endDate: null,
+    focusedInput: null
+  };
+
+  render() {
+    return (
+      <DateRangePicker
+        startDate={this.state.startDate} // momentPropTypes.momentObj or null,
+        startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
+        endDate={this.state.endDate} // momentPropTypes.momentObj or null,
+        endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
+        onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
+        focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+        onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
+        hideKeyboardShortcutsPanel
+        showClearDates
+        showDefaultInputIcon
+        small
+      />
+    );
+  }
+}
+
+export default App;
