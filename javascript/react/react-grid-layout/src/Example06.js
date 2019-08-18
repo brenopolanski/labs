d57790f4ec6f1d00baba546c003ec0723e65db13
brenopolanski <breno.polanski@gmail.com>
2019-08-18 14:28:01 -0300

    Update example

--- javascript/react/react-grid-layout/src/Example06.js
+++ javascript/react/react-grid-layout/src/Example06.js
@@ -4,18 +4,33 @@ import { Card, Icon } from 'antd';
 class Example06 extends Component {
   render() {
     return (
-      <div style={{ background: '#ececec', padding: '30px' }}>
+      <div style={{ background: '#fff', padding: '30px' }}>
         <Card
           size="small"
-          title="Small size card"
+          title={<span><Icon type="edit" key="edit" style={{ color: 'yellow' }} /> <span style={{ color: 'yellow', fontWeight: 'bold' }}>Sales</span></span>}
           extra={<a href="#">More</a>}
           style={{ width: 300 }}
-          headStyle={{ backgroundColor: 'blue' }}
+          headStyle={{ backgroundColor: '#fff', borderBottom: 'none' }}
+          bodyStyle={{ backgroundColor: '#ccc' }}
+          bordered={true}
         >
           <p>Card content</p>
           <p>Card content</p>
           <p>Card content</p>
         </Card>
+
+        <br />
+
+        <Card
+          style={{ width: 300, border: '2px solid blue' }}
+          cover={
+            <img
+              alt="example"
+              src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+            />
+          }
+          bodyStyle={{ padding: 0 }}
+        />
       </div>
     );
   }
