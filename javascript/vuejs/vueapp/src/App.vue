0dcafdec1b312401f21bf109c95fa4ba93ebf085
brenopolanski <breno.polanski@gmail.com>
2020-01-09 14:36:46 -0300

    Create a simple Vue.js

---
+++ javascript/vuejs/vueapp/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Users />
+  </div>
+</template>
+
+<script>
+import Users from "./components/Users";
+
+export default {
+  name: "app",
+  components: {
+    Users
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ javascript/vuejs/vueapp/src/components/Users.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <h3>Users:</h3>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Id</th>
+          <th scope="col">Name</th>
+          <th scope="col">Email</th>
+          <th scope="col">City</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="user in users" v-bind:key="user.id">
+          <th scope="row">{{user.id}}</th>
+          <td>{{user.name}}</td>
+          <td>{{user.email}}</td>
+          <td>{{user.address.city}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Users",
+  data() {
+    return {
+      users: null
+    };
+  },
+  created: function() {
+    axios.get("https://jsonplaceholder.typicode.com/users").then(res => {
+      this.users = res.data;
+    });
+  }
+};
+</script>
+
+<style>
+h3 {
+  margin-bottom: 5%;
+}
+</style>
