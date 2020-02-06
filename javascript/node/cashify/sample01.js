7ddce81c5cb4d08d0041dc5b3c954f6f83a764ac
brenopolanski <breno.polanski@gmail.com>
2020-02-06 17:03:55 -0300

    Add example using cashify

---
+++ javascript/node/cashify/sample01.js
@@ -0,0 +1,15 @@
+const {Cashify} = require('cashify');
+
+const rates = {
+  USD: 1.0000000000000000,
+  ARK: 0.2080325454335759,
+  EUR: 1.0975454493570578,
+  BRL: 0.2347053661391977,
+  BTC: 9719.5564583014163242,
+};
+
+const cashify = new Cashify({base: 'USD', rates});
+
+const result = cashify.convert(1, {from: 'BTC', to: 'ARK'});
+
+console.log(result);
