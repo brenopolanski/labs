e8d29a6edf616fa9cd2a36240e1bd3406b76dfbb
brenopolanski <breno.polanski@gmail.com>
2021-01-18 14:52:11 -0300

    Add a Blog App With Next.js, React Hooks, and Firebase Backend Tutorial

---
+++ javascript/nextjs/nextjs-firebase-blog/pages/users/register.js
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import fire from '../../config/fire-config';
+import { useRouter } from 'next/router';
+
+const Register = () => {
+  const router = useRouter();
+  const [userName, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [passConf, setPassConf] = useState('');
+  const [notification, setNotification] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    if (password !== passConf) {
+      setNotification(
+       'Password and password confirmation does not   match'
+      )
+      setTimeout(() => {
+        setNotification('')
+      }, 2000)
+      setPassword('');
+      setPassConf('');
+      return null;
+      }
+    fire.auth()
+      .createUserWithEmailAndPassword(userName, password)
+      .catch((err) => {
+        console.log(err.code, err.message)
+      });
+    router.push("/")
+  }
+
+  return (
+    <div>
+      <h1>Create new user</h1>
+        {notification}
+      <form onSubmit={handleLogin}>
+        Email: <input type="text" value={userName}
+        onChange={({target}) => setUsername(target.value)} />
+        <br />
+        Password: <input type="password" value={password}
+        onChange={({target}) => setPassword(target.value)} />
+        <br />
+        Password conf: <input type="password" value={passConf}
+        onChange={({target}) => setPassConf(target.value)} />
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  )
+}
+
+export default Register
