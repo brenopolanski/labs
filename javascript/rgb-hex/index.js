3f86c3dc7c47474da5c8b6b4cc4a4bbf36b4fb84
brenopolanski <breno.polanski@gmail.com>
2022-01-17 10:44:55 -0300

    add example rgb-hex

---
+++ javascript/rgb-hex/index.js
@@ -0,0 +1,12 @@
+// blackA90: 'rgba(0, 0, 0, 0.9)',
+// blueA10: 'rgba(18, 91, 255, 0.1)',
+// A100: 'rgba(117, 71, 220, 0.1)',
+// A200: 'rgba(117, 71, 220, 0.2)',
+// A400: 'rgba(117, 71, 220, 0.4)',
+// A700: 'rgba(117, 71, 220, 0.7)'
+
+import rgbHex from 'rgb-hex';
+
+const hex = rgbHex(18, 91, 255, 0.1)
+
+console.log(hex)
