e14bc90f952cdcb563ed37dee2e9adc1e5f38452
brenopolanski <breno.polanski@gmail.com>
2022-03-29 14:03:57 -0300

    add example web share api

---
+++ javascript/web-api/web-share/script.js
@@ -0,0 +1,21 @@
+const shareButton = document.querySelector('.share-button');
+const shareDialog = document.querySelector('.share-dialog');
+const closeButton = document.querySelector('.close-button');
+
+shareButton.addEventListener('click', event => {
+  if (navigator.share) {
+    navigator.share({
+      title: 'WebShare API Demo',
+      url: 'https://codepen.io/ayoisaiah/pen/YbNazJ'
+    }).then(() => {
+      console.log('Thanks for sharing!');
+    })
+    .catch(console.error);
+  } else {
+    shareDialog.classList.add('is-open');
+  }
+});
+
+closeButton.addEventListener('click', event => {
+  shareDialog.classList.remove('is-open');
+});
